d90102d3cce535d9ceb93ed341f0a7e4d73a16c6
kasai441 <kasai441441@gmail.com>
2021-12-22 14:20:54 +0900

    再マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200122083022) do
+ActiveRecord::Schema.define(version: 2020_01_22_083022) do
 
   create_table "studies", force: :cascade do |t|
     t.datetime "started_at"
